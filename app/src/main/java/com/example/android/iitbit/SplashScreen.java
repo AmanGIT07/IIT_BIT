6f14a2c60276062d2c16a9324f515bbe9310dd5b
Aman Prasad <aman.psd66@gmail.com>
2017-06-01 22:47:41 +0530

    Commit1

--- app/src/main/java/com/example/android/iitbit/SplashScreen.java
+++ app/src/main/java/com/example/android/iitbit/SplashScreen.java
@@ -1,13 +1,24 @@
 package com.example.android.iitbit;
 
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class SplashScreen extends AppCompatActivity {
-
+    ImageView imageView ;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+        imageView = (ImageView)findViewById(R.id.iv_splashScreen);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashScreen.this,Login.class);
+                startActivity(intent);
+            }
+        },1500);
     }
 }
