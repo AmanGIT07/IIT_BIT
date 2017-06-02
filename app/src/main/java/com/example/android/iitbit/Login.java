a0400bd1fbe534346eb5f8b8be927bcae92cf999
Aman Prasad <aman.psd66@gmail.com>
2017-06-02 11:15:18 +0530

    Commit5

--- app/src/main/java/com/example/android/iitbit/Login.java
+++ app/src/main/java/com/example/android/iitbit/Login.java
@@ -1,14 +1,38 @@
 package com.example.android.iitbit;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class Login extends Activity {
-
+public class Login extends AppCompatActivity {
+        Button register;
+        Intent intent;
+        TextView tv1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        tv1 = (TextView)findViewById(R.id.tv_forgotPassword);
+        tv1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                intent = new Intent(Login.this,ForgotPassword.class);
+                startActivity(intent);
+            }
+        });
+        register = (Button)findViewById(R.id.btn_createNewWallet);
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                intent = new Intent(Login.this,Register.class);
+                startActivity(intent);
+            }
+        });
     }
+
+
 }
