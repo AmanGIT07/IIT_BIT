a681ce7760db0016cc4d27ac195d8c29aeedaf91
Aman Prasad <aman.psd66@gmail.com>
2017-06-19 15:40:24 +0530

    Commit19 - Timer modified

--- app/src/main/java/com/example/android/iitbit/ForgotPassword.java
+++ app/src/main/java/com/example/android/iitbit/ForgotPassword.java
@@ -1,8 +1,11 @@
 package com.example.android.iitbit;
 
 import android.content.Intent;
+import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 import android.os.Handler;
 
@@ -10,45 +13,46 @@ import java.util.concurrent.TimeUnit;
 
 public class ForgotPassword extends AppCompatActivity {
     TextView tvShowTime;
-  private Handler handler;
-    int remTime;
+    int time = 10000;//5*60*1000;//milli seconds
     Intent intent;
+    Button goHome;
+    CountDownTimer countDownTimer;
 
     @Override
     public void onBackPressed() {
-        remTime = 0;
+        super.onBackPressed();
+        countDownTimer.cancel();
     }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_forgot_password);
-        remTime =5*60*1000;//milli seconds
+        goHome = (Button)findViewById(R.id.btn_goHomeFromForgotPass);
         tvShowTime =(TextView) findViewById(R.id.tv_showRemTime);
-        handler = new Handler();
 
+        countDownTimer  = new CountDownTimer(time, 1000) {
 
-        Runnable runnable = new Runnable() {
-            @Override
-            public void run() {
-                if(remTime>0)
-                {
-                    String ms = String.format("%02d:%02d", TimeUnit.MILLISECONDS.toMinutes(remTime) % TimeUnit.HOURS.toMinutes(1),
-                            TimeUnit.MILLISECONDS.toSeconds(remTime) % TimeUnit.MINUTES.toSeconds(1));
+            public void onTick(long millisUntilFinished) {
+                String ms = String.format("%02d:%02d", TimeUnit.MILLISECONDS.toMinutes(millisUntilFinished) % TimeUnit.HOURS.toMinutes(1),
+                            TimeUnit.MILLISECONDS.toSeconds(millisUntilFinished) % TimeUnit.MINUTES.toSeconds(1));
                     tvShowTime.setText(ms);
-                    remTime -= 1000;
-                    handler.postDelayed(this,1000);
-                }
-                else
-                {
-                    intent = new Intent(ForgotPassword.this,Login.class);
-                    intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-                    startActivity(intent);
-                    finish();
-                }
             }
-        };
 
-        handler.postDelayed(runnable,1000);
+            public void onFinish() {
+                intent = new Intent(ForgotPassword.this,Login.class);
+                startActivity(intent);
+            }
+
+        }.start();
+
+        goHome.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                countDownTimer.cancel();
+                intent = new Intent(ForgotPassword.this,Login.class);
+                startActivity(intent);
+            }
+        });
     }
 }
